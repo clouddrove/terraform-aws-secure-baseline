486d735a72004c72d6a4bdaad51d56ad7552a10d
Anmol Nagpal <anmol@clouddrove.com>
2021-11-09 20:48:05 +0530

    change lambda version

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -166,6 +166,8 @@ module "slack-lambda" {
   principals = [
     "events.amazonaws.com"
   ]
-  source_arns = [join("", aws_cloudwatch_event_rule.default.*.arn)]
-  variables   = var.variables
+  source_arns           = [join("", aws_cloudwatch_event_rule.default.*.arn)]
+  variables             = var.variables
+  tracing_mode          = "Active"
+  attach_tracing_policy = true
 }
